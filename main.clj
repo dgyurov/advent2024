92293c8143cd47e9bbe0890f3b6a84c38a70f98c
dgyurov <dgyurov@gmail.com>
2024-12-03 10:37:12 +0100

    Day 2

--- main.clj
+++ main.clj
@@ -1,24 +1,26 @@
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as str])
 
-; Read the input | O(n)
 (def lines
   (with-open [rdr (io/reader "input.txt")]
     (str/split-lines (slurp rdr))))
 
-(def lefts [])
-(def rights [])
+(def records [])
 
 (doseq [line lines]
-  (def numbers (map #(Integer/parseInt %) (str/split line #"   ")))
-  (def lefts (conj lefts (first numbers)))
-  (def rights (conj rights (second numbers))))
+  (def records (into records [(map #(Integer/parseInt %) (str/split line #" "))])))
 
-; Part 1 | O(nlogn)
-(def sorted-lefts (sort lefts))
-(def sorted-rights (sort rights))
-(println (reduce (fn [acc i] (+ acc (Math/abs (- (nth sorted-lefts i) (nth sorted-rights i))))) 0 (range (count lefts))))
+(defn is-record-valid [record]
+  (or (apply <= record) (apply >= record)))
 
-; Part 2 | O(n)
-(def right-counts (frequencies rights))
-(println (reduce (fn [acc currentNumber] (+ acc (* currentNumber (get right-counts currentNumber 0)))) 0 lefts))
+(defn is-record-safe [record] (let [pairs (map (fn [left, right] (let [diff (Math/abs (- left right))] (and (>= diff 1) (<= diff 3)))) record (rest record))] (every? true? pairs)))
+
+(defn is-record-safe-and-valid [record] (and (is-record-valid record) (is-record-safe record)))
+
+(defn is-alternative-safe-and-valid [record] (reduce (fn [acc i] (or acc (let [alternative-record (concat (take i record) (drop (inc i) record))] (and (is-record-valid alternative-record) (is-record-safe alternative-record))))) false (range (count record))))
+
+; Part 1
+(println (reduce (fn [acc record] (+ acc (if (is-record-safe-and-valid record) 1 0))) 0 records))
+
+; Part 2
+(println (reduce (fn [acc record] (+ acc (if (or (is-record-safe-and-valid record) (is-alternative-safe-and-valid record)) 1 0))) 0 records))
